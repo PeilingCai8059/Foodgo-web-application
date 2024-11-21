708f96c7a4ab60e69917019fae66be060e8c6185
Peiling <jxg9@txstate.edu>
2024-11-21 00:51:32 -0600

    Add seach feature

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../src/assets/assets.js'
 import { Link, useNavigate } from 'react-router-dom';
@@ -6,29 +6,40 @@ import { StoreContext } from '../../context/StoreContext';
 import { GoPerson } from "react-icons/go";
 import { FiShoppingBag } from "react-icons/fi";
 import { MdLogout } from "react-icons/md";
+import { MdOutlineClear } from "react-icons/md";
 
 // eslint-disable-next-line react/prop-types
 const Navbar = ({ setShowlogin }) => {
-  const [menu, setMenu] = useState("home");
-  const { getTotal, token, setToken } = useContext(StoreContext);
+  const { getTotal, token, setToken,menu, setMenu, setSearchString } = useContext(StoreContext);
   const navigate = useNavigate();
+  const [searchFoodName, setSearchFoodName] = useState("");
 
   function logout() {
     localStorage.removeItem("token");
     setToken("");
     navigate('/')
   }
+  function handleSearch(){
+    setSearchString (searchFoodName);
+  }
+
   return (
     <div className='navbar'>
       <Link to="/"><img src={assets.logo} alt="logo" className='logo' /></Link>
       <ul className="navbar-menu">
         <Link to='/' onClick={() => setMenu("home")} className={menu === 'home' ? "active" : ""}>home</Link>
-        <a href='/menu' onClick={() => setMenu("menu")} className={menu === 'menu' ? "active" : ""}>menu</a>
+        <Link to='/menu' onClick={() => setMenu("menu")} className={menu === 'menu' ? "active" : ""}>menu</Link>
         <a href='/#app-download' onClick={() => setMenu("moblile-app")} className={menu === 'moblile-app' ? "active" : ""}>moblile-app</a>
         <a href='/#footer' onClick={() => setMenu("contact-us")} className={menu === 'contact-us' ? "active" : ""}>contact-us</a>
       </ul>
       <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
+        <div className='navbar-search'>
+          <input type="text" onChange={(e)=> setSearchFoodName(e.target.value)} placeholder='Search' value={searchFoodName} />
+          <div className="search-icon">
+             <img onClick={handleSearch} src={assets.search_icon}  />
+             {searchFoodName && < MdOutlineClear onClick={()=> setSearchFoodName("")} className='search-icon-delete' />}
+          </div>
+        </div>
         <div className="navbar-search-icon">
           <Link to='/cart'> <img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotal() === 0 ? "" : "dot"}></div>
